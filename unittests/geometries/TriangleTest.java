9f662931a35d1a9ec038f10df14dce716aeca553
ester garada <ester.g.9565@gmail.com>
2025-03-30 00:53:51 +0300

    tests for level 2.1

--- unittests/geometries/TriangleTest.java
+++ unittests/geometries/TriangleTest.java
@@ -1,7 +1,45 @@
 package geometries;
 
+import org.junit.jupiter.api.Test;
+import primitives.*;
+import primitives.Ray;
+import primitives.Vector;
+//import geometries.Intersectable.GeoPoint;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * Unit tests for the Triangle class.
+ */
 class TriangleTest {
+    /**
+     * Delta value for accuracy when comparing the numbers of type 'double' in
+     * assertEquals
+     */
+    private final double DELTA = 0.00001;
+
+    /**
+     * Test case for getting the normal vector of a triangle.
+     */
+    @Test
+    void testGetNormal() {
+        // ============ Equivalence Partitions Tests ==============
+        // TC01: Test the normal vector calculation of a triangle defined by three points
+        Triangle triangle = new Triangle(new Point(0, 0, 0), new Point(1, 0, 0),
+                new Point(0, 1, 0));
+        Vector result = triangle.getNormal(new Point(0, 0, 0));
+        // Test that the length of the normal is 1
+        assertEquals(1, result.length(), DELTA, "ERROR: the length of the normal is not 1");
+        // Check that the normal vector is orthogonal to the plane
+        assertEquals(0.0, result.dotProduct(new Vector(1, 0, 0)), DELTA,
+                "ERROR: the normal vector is not orthogonal to the vector (1, 0, 0)");
+        assertEquals(0.0, result.dotProduct(new Vector(0, 1, 0)), DELTA,
+                "ERROR: the normal vector is not orthogonal to the vector (0, 1, 0)");
+        // Check that the normal vector is as expected
+        assertEquals(new Vector(0, 0, 1), result,
+                "getNormal() did not return the expected normal vector.");
+    }
 
 }
