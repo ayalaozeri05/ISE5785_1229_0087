817b65c2c590ca460af2f92bf6bd0095ae1037b2
ayalaozeri05 <aozeri@g.jct.ac.il>
2025-06-22 18:25:42 +0300

    minip1

--- src/renderer/Camera.java
+++ src/renderer/Camera.java
@@ -1,6 +1,10 @@
 package renderer;
 import primitives.*;
 import scene.Scene;
+
+
+import java.util.LinkedList;
+import java.util.List;
 import java.util.MissingResourceException;
 
 
@@ -22,34 +26,52 @@ public class Camera implements Cloneable {
     public int nY=1;
 
 
+    private BlackBoard blackBoard;
+
+
     /**
-     * Cast a ray through a pixel
-     * @param nx the number of pixels in the x direction
-     * @param ny the number of pixels in the y direction
-     * @param i the y index of the pixel
-     * @param j the x index of the pixel
+     * Casts a single ray through a specific pixel and writes its color to the image.
+     *
+     * @param j pixel column index
+     * @param i pixel row index
      */
-    private void castRay(int nx, int ny, int i, int j) {
-        Ray ray = constructRay(nx, ny, j, i);
-        Color color = rayTracer.traceRay(ray);
-        imageWriter.writePixel(j, i, color);
+    private void castRay(int j, int i) {
+        int nX = this.nX;
+        int nY = this.nY;
+        Color color = Color.BLACK;
 
-    }
-
-
-/**
- * Render the image
- */
-public Camera renderImage() {
-    int ny = imageWriter.nY();
-    int nx = imageWriter.nX();
-    for (int i = 0; i < ny; i++) {
-        for (int j = 0; j < nx; j++) {
-            castRay(nx, ny, i, j);
+        if (blackBoard.getIsAntiAliasingEnabled()) {
+            List<Ray> rays = constructRays(nX, nY, j, i);
+            for (Ray ray : rays) {
+                color = color.add(rayTracer.traceRay(ray));
+            }
+            color = color.reduce(rays.size());
+        } else {
+            Ray ray = constructRay(nX, nY, j, i);
+            color = rayTracer.traceRay(ray);
         }
+
+        imageWriter.writePixel(j, i, color);
+     //   pixelManager.pixelDone();
     }
-    return this;
-}
+
+
+    /**
+     * Renders the image by casting rays through all pixels and computing their color.
+     *
+     * @return this camera after rendering
+     */
+    public Camera renderImage() {
+        int ny = imageWriter.nY();
+        int nx = imageWriter.nX();
+        for (int i = 0; i < ny; i++) {
+            for (int j = 0; j < nx; j++) {
+                castRay(j, i);
+            }
+        }
+        return this;
+    }
+
 
     /**
      * Print a grid on the image
@@ -76,6 +98,7 @@ public Camera renderImage() {
         imageWriter.writeToImage(fileName); // assuming imageWriter has a method that accepts a file name
         return this;
     }
+
 
     /**
      * Camera getter
@@ -135,12 +158,27 @@ public Camera renderImage() {
         return distance;
     }
 
+    /**
+     * Returns the center point of the view plane (pixel grid),
+     * located at a fixed distance from the camera position in the direction of view.
+     * This point serves as the reference for generating rays toward pixels.
+     *
+     * @return the center point of the view plane.
+     */
+    public Point getPcenter() {
+        return p0.add(vTo.scale(distance));
+    }
+
+
 
     /**
      * Camera builder
      */
     public static class Builder {
         private final Camera camera = new Camera();
+
+        private BlackBoard blackBoard;
+
 
 
         /**
@@ -257,6 +295,55 @@ public Camera renderImage() {
         }
 
         /**
+         * Sets the BlackBoard instance used for anti-aliasing configuration.
+         * The BlackBoard defines the sampling grid size and whether anti-aliasing is enabled.
+         *
+         * @param blackBoard the BlackBoard object containing anti-aliasing settings
+         * @return this builder for method chaining
+         */
+        public Builder setBlackBoard(BlackBoard blackBoard) {
+            this.blackBoard = blackBoard;
+            return this;
+        }
+
+
+
+//        /**
+//         * Sets the boolean field  anti-aliasing to be true or false .
+//         *
+//         * @param antiAliasing the boolean value- true or false.
+//         * @return the builder instance.
+//         */
+//        public Builder setAntiAliasing(boolean antiAliasing) {
+//            camera.antiAliasing = antiAliasing;
+//            return this;
+//        }
+
+//        /**
+//         * Sets the number of rays for anti-aliasing.
+//         *
+//         * @param numberOfRays the number of rays to set.
+//         * @return the builder instance.
+//         * @throws IllegalArgumentException if the number of rays is negative or zero.
+//         */
+//        public Builder setAntiAliasingNumberOfRays(int numberOfRays) {
+//            camera.ANTI_ALIASING_NUMBER_OF_RAYS = numberOfRays;
+//            return this;
+//        }
+//        /**
+//         * Sets the number of points in the aperture of the camera.
+//         *
+//         * @param numberOfPoints the number of points to set.
+//         * @return the builder instance.
+//         * @throws IllegalArgumentException if the number of points is negative or zero.
+//         */
+//        public Builder setApertureNumberOfPoints(int numberOfPoints ) {
+//            camera.APERTURE_NUMBER_OF_POINTS=numberOfPoints;
+//            return this;
+//
+//        }
+
+        /**
          * Build the camera
          *
          * @return the camera
@@ -306,6 +393,8 @@ public Camera renderImage() {
             if (camera.distance <= 0)
                 throw new IllegalArgumentException("distance from camera to view must be positive");
 
+            camera.blackBoard = this.blackBoard;
+
             try {
                 return (Camera) camera.clone();
             } catch (CloneNotSupportedException e) {
@@ -314,7 +403,10 @@ public Camera renderImage() {
         }
 
 
+
     }
+
+
 
     /**
      * Camera constructor
@@ -340,18 +432,92 @@ public Camera renderImage() {
      * @param i  the y index of the pixel
      * @return the ray that passes through the pixel
      */
+    /**
+     * Constructs a single ray from the camera origin through the center of a pixel.
+     *
+     * @param nX number of pixels in X direction
+     * @param nY number of pixels in Y direction
+     * @param j  pixel column index
+     * @param i  pixel row index
+     * @return a {@link Ray} from the camera origin through the pixel center
+     */
     public Ray constructRay(int nX, int nY, int j, int i) {
-        Point pIJ = p0;
-        double yI = -(i - (nY - 1) / 2d) * height / nY;
-        double xJ = (j - (nX - 1) / 2d) * width / nX;
+        double pixelWidth = width / nX;
+        double pixelHeight = height / nY;
 
-        //check if xJ or yI are not zero, so we will not add zero vector
-        if (!Util.isZero(xJ)) pIJ = pIJ.add(vRight.scale(xJ));
-        if (!Util.isZero(yI)) pIJ = pIJ.add(vUp.scale(yI));
+        double xJ = (j - (nX - 1) / 2.0) * pixelWidth;
+        double yI = (i - (nY - 1) / 2.0) * pixelHeight;
 
-        // we need to move the point in the direction of vTo by distance
-        pIJ = pIJ.add(vTo.scale(distance));
+        Point pIJ = p0.add(vTo.scale(distance));
+        if (xJ != 0) {
+            pIJ = pIJ.add(vRight.scale(xJ));
+        }
+        if (yI != 0) {
+            pIJ = pIJ.add(vUp.scale(-yI));
+        }
 
-        return new Ray(p0, pIJ.subtract(p0).normalize());
+        return new Ray(p0, pIJ.subtract(p0));
     }
+
+    /**
+     * Constructs multiple rays through a pixel for anti-aliasing.
+     *
+     * @param nX number of pixels in X direction
+     * @param nY number of pixels in Y direction
+     * @param j  pixel column index
+     * @param i  pixel row index
+     * @return list of {@link Ray} through the pixel with jittered sampling
+     */
+    public List<Ray> constructRays(int nX, int nY, int j, int i) {
+        double pixelWidth = width / nX;
+        double pixelHeight = height / nY;
+
+        double xJ = (j - (nX - 1) / 2.0) * pixelWidth;
+        double yI = (i - (nY - 1) / 2.0) * pixelHeight;
+
+        return blackBoard.sampleJittered(this, pixelWidth, pixelHeight, xJ, yI);
+    }
+
+
+//    private Color calcAveragePixelColor(int nX, int nY, int j, int i) {
+//        double pixelWidth = width / nX;
+//        double pixelHeight = height / nY;
+//
+//        Point pCenter = p0.add(vTo.scale(distance));
+//        double xJ = (j - (nX - 1) / 2.0) * pixelWidth;
+//        double yI = -(i - (nY - 1) / 2.0) * pixelHeight;
+//
+//        if (!Util.isZero(xJ)) pCenter = pCenter.add(vRight.scale(xJ));
+//        if (!Util.isZero(yI)) pCenter = pCenter.add(vUp.scale(yI));
+//
+//        List<Ray> rays = generateRayGrid(pCenter, pixelWidth, pixelHeight, ANTI_ALIASING_NUMBER_OF_RAYS);
+//
+//        Color color = Color.BLACK;
+//        for (Ray ray : rays) {
+//            color = color.add(rayTracer.traceRay(ray));
+//        }
+//        return color.reduce(rays.size());
+//    }
+
+//    private List<Ray> generateRayGrid(Point center, double pixelWidth, double pixelHeight, int numRaysPerDim) {
+//        List<Ray> rays = new LinkedList<>();
+//        double subPixelWidth = pixelWidth / numRaysPerDim;
+//        double subPixelHeight = pixelHeight / numRaysPerDim;
+//
+//        for (int i = 0; i < numRaysPerDim; i++) {
+//            for (int j = 0; j < numRaysPerDim; j++) {
+//                double xOffset = (j + 0.5 - numRaysPerDim / 2.0) * subPixelWidth;
+//                double yOffset = -(i + 0.5 - numRaysPerDim / 2.0) * subPixelHeight;
+//
+//                Point p = center;
+//                if (!Util.isZero(xOffset)) p = p.add(vRight.scale(xOffset));
+//                if (!Util.isZero(yOffset)) p = p.add(vUp.scale(yOffset));
+//
+//                rays.add(new Ray(p0, p.subtract(p0)));
+//            }
+//        }
+//        return rays;
+//    }
+
+
 }
